42753fcd4a8b10ea1539883efffdf71268151779
AntBezr <sosleepyanton@gmail.com>
2023-04-11 09:32:54 +0300

    styling

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Footer.css'
 
 function Footer(props) {
   return (
-    <div>
-      footer will be here
-    </div>
+    <footer>
+      <small>React23K Anton Bezruchenkov ©</small>
+      
+    </footer>
   );
 }
 
